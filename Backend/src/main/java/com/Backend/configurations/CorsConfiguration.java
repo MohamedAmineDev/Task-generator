8669e4797dc6a39cc7dea65952963deb7f7ccb88
Mohamed Amine Ben Safta <mohamedaminebensafta@gmail.com>
2024-01-11 22:37:17 +0100

    Dockerfiles pushed to docker hub

--- Backend/src/main/java/com/Backend/configurations/CorsConfiguration.java
+++ Backend/src/main/java/com/Backend/configurations/CorsConfiguration.java
@@ -8,10 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://0.0.0.0:3000")
+         registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
